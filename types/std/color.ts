96b010f28197855a54b5825b845b8bec065d63f5
Harry Solovay <harrysolovay@gmail.com>
2024-11-25 20:28:35 -0500

    Fix Slow Types

--- types/std/color.ts
+++ types/std/color.ts
@@ -1,14 +1,26 @@
 import { object } from "../object.ts"
 import { number } from "../primitives.ts"
 import { transform } from "../transform.ts"
+import type { Ty } from "../Ty.ts"
 
-export const Rgb = object({
+export const Rgb: Ty<
+  {
+    r: number
+    g: number
+    b: number
+  },
+  never,
+  true
+> = object({
   r: number`Between 0 and 255`,
   g: number`Between 0 and 255`,
   b: number`Between 0 and 255`,
 })
 
-export const Hex = transform(Rgb, ({ r, g, b }) => `${toHex(r)}${toHex(g)}${toHex(b)}"`)
+export const Hex: Ty<string, never, true> = transform(
+  Rgb,
+  ({ r, g, b }) => `${toHex(r)}${toHex(g)}${toHex(b)}"`,
+)
 
 function toHex(value: number): string {
   return value.toString(16).padStart(2, "0")
