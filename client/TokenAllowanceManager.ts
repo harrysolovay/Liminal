494b1cf39e7d7d73cea3379a3e516b23cd75fa9b
Harry Solovay <harrysolovay@gmail.com>
2024-12-03 09:30:22 -0500

    tweaks

--- client/TokenAllowanceManager.ts
+++ client/TokenAllowanceManager.ts
@@ -2,17 +2,17 @@ import type { ChatCompletion } from "openai/resources/chat/completions"
 
 export interface TokenAllowances {
   /** Number of tokens in the generated completion. */
-  completion_tokens: number
+  completion_tokens?: number
   /** Number of tokens in the prompt. */
-  prompt_tokens: number
+  prompt_tokens?: number
   /** Total number of tokens used in the request (prompt + completion). */
-  total_tokens: number
+  total_tokens?: number
 }
 
 export class TokenAllowanceManager {
   stop: boolean = false
   constructor(
-    readonly allowances: Partial<TokenAllowances>,
+    readonly allowances: TokenAllowances,
     readonly halt?: (self: TokenAllowanceManager) => boolean,
   ) {}
 
@@ -20,8 +20,7 @@ export class TokenAllowanceManager {
     const { usage } = completion
     if (usage) {
       Object.keys(this.allowances).forEach((key) => {
-        this.allowances[key as keyof TokenAllowances]! -= usage[key as never]
-        if (this.allowances[key as keyof TokenAllowances]! <= 0) {
+        if (!(this.allowances[key as keyof TokenAllowances]! -= usage[key as never])) {
           this.stop = true
         }
       })
