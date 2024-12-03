90ea9d6f98cacdfb0a82aefb00083cacd5be7552
Harry Solovay <harrysolovay@gmail.com>
2024-12-03 12:15:33 -0500

    upate test snapshots

--- json/toSchema.test.ts
+++ json/toSchema.test.ts
@@ -2,50 +2,52 @@ import { assertSnapshot } from "@std/testing/snapshot"
 import { T, type Type } from "../mod.ts"
 import { toSchema } from "./toSchema.ts"
 
-Deno.test("boolean", async (t) => {
-  await assertTypeSnapshot(t, T.boolean)
-})
+Deno.test("toSchema", async (t) => {
+  await t.step("boolean", async (t) => {
+    await assertTypeSnapshot(t, T.boolean)
+  })
 
-Deno.test("number", async (t) => {
-  await assertTypeSnapshot(t, T.number)
-})
+  await t.step("number", async (t) => {
+    await assertTypeSnapshot(t, T.number)
+  })
 
-Deno.test("string", async (t) => {
-  await assertTypeSnapshot(t, T.string)
-})
+  await t.step("string", async (t) => {
+    await assertTypeSnapshot(t, T.string)
+  })
 
-Deno.test("array", async (t) => {
-  await assertTypeSnapshot(t, T.array(T.string))
-})
+  await t.step("array", async (t) => {
+    await assertTypeSnapshot(t, T.array(T.string))
+  })
 
-Deno.test("object", async (t) => {
-  await assertTypeSnapshot(
-    t,
-    T.object({
-      a: T.boolean,
-      b: T.number,
-      c: T.string,
-    }),
-  )
-})
+  await t.step("object", async (t) => {
+    await assertTypeSnapshot(
+      t,
+      T.object({
+        a: T.boolean,
+        b: T.number,
+        c: T.string,
+      }),
+    )
+  })
 
-Deno.test("option", async (t) => {
-  await assertTypeSnapshot(t, T.option(T.string))
-})
+  await t.step("option", async (t) => {
+    await assertTypeSnapshot(t, T.option(T.string))
+  })
 
-Deno.test("enum", async (t) => {
-  await assertTypeSnapshot(t, T.enum("A", "B", "C"))
-})
+  await t.step("enum", async (t) => {
+    await assertTypeSnapshot(t, T.enum("A", "B", "C"))
+  })
 
-Deno.test("tagged union", async (t) => {
-  await assertTypeSnapshot(
-    t,
-    T.taggedUnion("type", {
-      A: T.boolean,
-      B: T.number,
-      C: T.string,
-    }),
-  )
+  await t.step("tagged union", async (t) => {
+    await assertTypeSnapshot(
+      t,
+      T.taggedUnion("type", {
+        A: T.boolean,
+        B: T.number,
+        C: T.string,
+      }),
+    )
+  })
 })
 
 async function assertTypeSnapshot(t: Deno.TestContext, value: Type<any>): Promise<void> {
