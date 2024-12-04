ca47371881754ba0de5dc56736af897274548a32
Harry Solovay <harrysolovay@gmail.com>
2024-12-04 08:46:39 -0500

    update tests

--- util/mod.ts
+++ util/mod.ts
@@ -2,10 +2,10 @@ export * as asserts from "./asserts/mod.ts"
 
 // moderate --exclude asserts
 
-export * from "./assert.ts"
-export * from "./collect.ts"
-export * from "./dbg.ts"
-export * from "./recombine.ts"
-export * from "./splitLast.ts"
-export * from "./tap.ts"
-export * from "./type_util.ts"
+export * from "./assert.ts";
+export * from "./collect.ts";
+export * from "./dbg.ts";
+export * from "./recombine.ts";
+export * from "./splitLast.ts";
+export * from "./tap.ts";
+export * from "./type_util.ts";
